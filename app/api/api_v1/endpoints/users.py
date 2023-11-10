cf63efde5cc991239227601d2236ddd62e49827a
mabrishami00 <mabrishami78@yahoo.com>
2023-11-10 08:44:12 +0330

    feat: add login

--- app/api/api_v1/endpoints/users.py
+++ app/api/api_v1/endpoints/users.py
@@ -41,3 +41,21 @@ async def user_register(user_data: UserCreate):
                 {"detail": "invalid request."}, status_code=status.HTTP_400_BAD_REQUEST
             )
 
+
+@router.post("/login/", status_code=status.HTTP_200_OK)
+async def user_login(user_data: UserLogin):
+    user_data_dict = user_data.model_dump()
+    async with httpx.AsyncClient() as client:
+        url = settings.LOGIN_URL
+        response = await client.post(url, json=user_data_dict)
+        response_dict = json.loads(response.text)
+        if response.status_code == 200:
+            return JSONResponse(response_dict)
+        elif response.status_code == 400:
+            return JSONResponse(response_dict, status_code=status.HTTP_400_BAD_REQUEST)
+        else:
+            return JSONResponse(
+                {"message": "invalid request."}, status_code=status.HTTP_400_BAD_REQUEST
+            )
+
+
